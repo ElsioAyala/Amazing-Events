d4e0708405caa05a0061f6673b9dab6e969c55e3
ElsioAyala <elsio_racing@hotmail.com>
2023-02-24 14:40:54 -0300

    task2

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,4 +1,3 @@
-
 const navbar = document.querySelector(".navbar");
 const btnToggle = document.getElementById("toggle-dark");
 
@@ -7,10 +6,10 @@ const btnToggle = document.getElementById("toggle-dark");
  * NAVBAR
  */
 window.addEventListener("scroll", () => {
-    if (scrollY >= 60 ) {
-        navbar.classList.add("navbar-scrolled"); 
+    if (scrollY >= 60) {
+        navbar.classList.add("navbar-scrolled");
         btnToggle.classList.contains('bi-sun') ? navbar.classList.remove("navbar-dark") : navbar.classList.add("navbar-dark");
-    }else {
+    } else {
         navbar.classList.remove("navbar-scrolled");
         navbar.classList.add("navbar-dark");
     }
@@ -21,11 +20,18 @@ window.addEventListener("scroll", () => {
  * DARK MODE
  */
 btnToggle.addEventListener("click", () => {
-    if (btnToggle.classList.contains('bi-sun')){
+    if (btnToggle.classList.contains('bi-sun')) {
         document.body.classList.add("dark-theme");
-        btnToggle.setAttribute("class", " bi bi-moon-stars");  
-    }else{
+        btnToggle.setAttribute("class", " bi bi-moon-stars");
+        localStorage.setItem("dark-mode", "true");
+    } else {
         document.body.classList.remove("dark-theme");
         btnToggle.setAttribute("class", " bi bi-sun");
+        localStorage.setItem("dark-mode", "false");
     }
-}); 
+});
+
+if (localStorage.getItem("dark-mode") === "true") {
+    document.body.classList.add("dark-theme");
+    btnToggle.setAttribute("class", " bi bi-moon-stars");
+}
