60af528b7295baedf7b3d2ba86410aedb09e0601
ElsioAyala <elsio_racing@hotmail.com>
2023-02-28 15:04:46 -0300

    higher order functions are added (forEach and filter)

--- scripts/pastEvents.js
+++ scripts/pastEvents.js
@@ -1,6 +1,7 @@
 const cardsContainer = document.getElementById("cards");
 const currentDate = setDate(data.currentDate);
-const cardsGenerated = createCards(data.events);
+const pastEvents = setPastEvents(data.events);
+const cardsGenerated = createCards(pastEvents);
 cardsContainer.innerHTML = cardsGenerated;
 
 
@@ -10,29 +11,28 @@ function setDate(date) {
     return dateOk.getTime();
 }
 
-
-function createCards(arrayDataEvents) {
-    let cardContent = ``;
-    for (const event of arrayDataEvents) {
-        const eventDate = setDate(event.date);
-        if (eventDate < currentDate) {
-            cardContent += `
-                <div class="col">
-                <div class="card h-100">
-                    <img src="${event.image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${event.name}</h5>
-                        <p class="card-text">${event.description}</p>
-                    </div>
-                    <div class="card-footer d-flex justify-content-between">
-                        <div class="card-price">$${event.price}</div>
-                        <a class="card-moreInfo" href="./details.html" target="_blank">More Info</a>
-                    </div>
-                </div>
-                </div>`;
-        }
-    }
-    return cardContent;
+function setPastEvents(arrayDataEvents) {
+    let arrayPastEvents = arrayDataEvents.filter(event => setDate(event.date) < currentDate);
+    return arrayPastEvents;
 }
 
-
+function createCards(arrayPastEvents) {
+    let cardContent = ``;
+    arrayPastEvents.forEach(event => {
+        cardContent += `
+            <div class="col">
+            <div class="card h-100">
+                <img src="${event.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${event.name}</h5>
+                    <p class="card-text">${event.description}</p>
+                </div>
+                <div class="card-footer d-flex justify-content-between">
+                    <div class="card-price">$${event.price}</div>
+                    <a class="card-moreInfo" href="./details.html" target="_blank">More Info</a>
+                </div>
+            </div>
+            </div>`;
+    });
+    return cardContent;
+}
--- scripts/upcomingEvents.js
+++ scripts/upcomingEvents.js
@@ -1,6 +1,7 @@
 const cardsContainer = document.getElementById("cards");
 const currentDate = setDate(data.currentDate);
-const cardsGenerated = createCards(data.events);
+const upcomingEvents = setUpcomingEvents(data.events);
+const cardsGenerated = createCards(upcomingEvents);
 cardsContainer.innerHTML = cardsGenerated;
 
 
@@ -10,27 +11,33 @@ function setDate(date) {
     return dateOk.getTime();
 }
 
+function setUpcomingEvents(arrayDataEvents) {
+    let arrayUpcomingEvents = arrayDataEvents.filter(event => setDate(event.date) > currentDate);
+    return arrayUpcomingEvents;
+}
 
-function createCards(arrayDataEvents) {
+
+function createCards(arrayUpcomingEvents) {
     let cardContent = ``;
-    for (const event of arrayDataEvents) {
-        const eventDate = setDate(event.date);
-        if (eventDate > currentDate) {
-            cardContent += `
-                <div class="col">
-                <div class="card h-100">
-                    <img src="${event.image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${event.name}</h5>
-                        <p class="card-text">${event.description}</p>
-                    </div>
-                    <div class="card-footer d-flex justify-content-between">
-                        <div class="card-price">$${event.price}</div>
-                        <a class="card-moreInfo" href="./details.html" target="_blank">More Info</a>
-                    </div>
+    arrayUpcomingEvents.forEach(event => {
+        cardContent += `
+            <div class="col">
+            <div class="card h-100">
+                <img src="${event.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${event.name}</h5>
+                    <p class="card-text">${event.description}</p>
                 </div>
-                </div>`;
-        }
-    }
+                <div class="card-footer d-flex justify-content-between">
+                    <div class="card-price">$${event.price}</div>
+                    <a class="card-moreInfo" href="./details.html" target="_blank">More Info</a>
+                </div>
+            </div>
+            </div>`;
+    });
     return cardContent;
 }
+
+
+
+
